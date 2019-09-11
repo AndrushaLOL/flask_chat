c7c73c79fa365585e49fe34b0b53796fdb1309d6
Andrey Nebogatikov <takemyenergy55@gmail.com>
2019-09-11 16:59:11 +0300

    friends

--- app/resourses.py
+++ app/resourses.py
@@ -103,7 +103,7 @@ class UserApi(Resource):
         try:
             return succes(**u.serialize, friends=[f.serialize for f in u.friends])
         except Exception as e:
-            return error(message=e.message)
+            return error(message='Something went wrong')
 
     
     def delete(self, username):
